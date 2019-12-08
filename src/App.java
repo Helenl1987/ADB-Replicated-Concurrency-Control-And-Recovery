be89cf9be54cc4e19730e5e45c4e5c77772e18e0
helen <519577403@qq.com>
2019-12-08 16:12:49 -0500

    add reading from stdin

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 
 public class App {
 	public static void main(String args[]) {
@@ -10,13 +11,21 @@ public class App {
 		}
 		TransactionManager TM = new TransactionManager(DM);
 		
-		String filename = "/Users/Helen/Documents/workspacejava/ADB-Replicated-Concurrency-Control-And-Recovery/test/Test21";
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(filename));
-			TM.Run(reader);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (args.length > 0) {
+			String filename = args[0];
+//			String filename = "/Users/Helen/Documents/workspacejava/ADB-Replicated-Concurrency-Control-And-Recovery/test/Test6";
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(filename));
+				TM.Run(reader);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		else {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			TM.Run(reader);
+		}
+
 	}
 }
